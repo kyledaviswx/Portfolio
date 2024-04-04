f022df63a42b64a6f8931fc421484710dd52a411
Kyle Davis <kdavis@instructors.2u.com>
2024-04-04 12:10:07 -0500

    Remove few references to old contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,49 +1,48 @@
-import React, { useState } from "react";
-import { validateEmail } from "../../utils/helpers";
+import React from "react";
 
 function ContactForm() {
-  // create form state and initial set up of name, email, message.
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const { name, email, message } = formState;
-  //create error message state
-  const [errorMessage, setErrorMessage] = useState("");
+  // // create form state and initial set up of name, email, message.
+  // const [formState, setFormState] = useState({
+  //   name: "",
+  //   email: "",
+  //   message: "",
+  // });
+  // const { name, email, message } = formState;
+  // //create error message state
+  // const [errorMessage, setErrorMessage] = useState("");
 
-  // function to manage a change in data of the form. Is called onBlur from each input
-  function handleChange(e) {
-    // validation for email, name, and description. Making sure email is valid and all 3 are required.
-    if (e.target.name === "email") {
-      const isValid = validateEmail(e.target.value);
+  // // function to manage a change in data of the form. Is called onBlur from each input
+  // function handleChange(e) {
+  //   // validation for email, name, and description. Making sure email is valid and all 3 are required.
+  //   if (e.target.name === "email") {
+  //     const isValid = validateEmail(e.target.value);
 
-      // isValid conditional statement
-      if (!isValid) {
-        setErrorMessage("Your email is invalid.");
-      } else {
-        setErrorMessage("");
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage("");
-      }
-    }
+  //     // isValid conditional statement
+  //     if (!isValid) {
+  //       setErrorMessage("Your email is invalid.");
+  //     } else {
+  //       setErrorMessage("");
+  //     }
+  //   } else {
+  //     if (!e.target.value.length) {
+  //       setErrorMessage(`${e.target.name} is required.`);
+  //     } else {
+  //       setErrorMessage("");
+  //     }
+  //   }
 
-    // setting the form state if there is no error and the form is correctly filled out.
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-    }
-  }
+  //   // setting the form state if there is no error and the form is correctly filled out.
+  //   if (!errorMessage) {
+  //     setFormState({ ...formState, [e.target.name]: e.target.value });
+  //   }
+  // }
 
-  // Sumbit button was clicked. This is where the developer would continue to integrate into the backend.
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("Form ready to submit. Here are the values...");
-    console.log(formState);
-  }
+  // // Sumbit button was clicked. This is where the developer would continue to integrate into the backend.
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   console.log("Form ready to submit. Here are the values...");
+  //   console.log(formState);
+  // }
 
   return (
     <section>
