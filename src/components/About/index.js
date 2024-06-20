af49b39ce4611d7f1b3987c127702dac9a329f95
kyledaviswx <kyledaviswx@gmail.com>
2024-06-19 19:53:19 -0500

    Update index.js - New text in bio paragraph

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -19,8 +19,8 @@ function About() {
               Graduated from the University of Oklahoma with a Bachelors of Science in Meteorology. Also received minors in Mathematics and Broadcast Journalism. I started my career at Weather Decision Technologies, Inc. in Norman, OK (Now owned by DTN). I started web development for an interactive weather map (iMap) at WDT.
               I then changed my career path and joined Epic Tech LLC and created Redibids.com. It is an auction style site with casino like gaming mixed in.
               Next, I spent 3 wonderful years at the Oklahoma Climatological Survey/Oklahoma Mesonet. I built a wide array of sites and tools used in meteorology, emergency management, and fire management.
-              I now work for the University of Oklahoma Libraries web team. We create and maintain the OU libraries site as well as numerous websites and tools used by the university libraries.
-              Also in 2022, I am excited to begin my time as an instructor for the online Full Stack Flex bootcamp at the University of Minnesota!
+              I am now the web team lead at the University of Oklahoma Libraries. We create and maintain the OU libraries site as well as numerous websites and tools used by the university libraries.
+              In 2022, I had an amazin experience as the instructor for the online Full Stack Flex bootcamp at the University of Minnesota. Teaching a wonderful group of students a wide variety of languages including REACT, Node.js, Express.js, and the MERN stack. It was challenging since most students were completely new, but it was very rewarding to see them become full stack programmers!
             </p>
           </div>
         </Col>
